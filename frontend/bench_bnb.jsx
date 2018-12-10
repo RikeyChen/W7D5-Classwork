55c413a6a79a73d816c18a1521ec42d7fc16a40c
Rikey Chen <rikey.chen16@gmail.com>
2018-12-10 12:20:59 -0800

    Install npm lodash and update reducers to import merge correctly

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
-import { login, logout } from './util/session_api_util';
+import { login, logout } from './actions/session_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -1,10 +1,11 @@
-import { merge } from 'lodash/merge';
+import { merge } from 'lodash';
 import { RECEIVE_CURRENT_USER } from '../actions/session_actions';
 
 const usersReducer = (oldState = {}, action) => {
   Object.freeze(oldState);
   switch (action.type) {
     case RECEIVE_CURRENT_USER:
+      // debugger;
       return merge({}, oldState, { [action.currentUser.id]: action.currentUser });
     default:
       return oldState;
