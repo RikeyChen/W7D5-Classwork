56162b8a58761c879b34ca929dc3a9b79f52c946
Rikey Chen <rikey.chen16@gmail.com>
2018-12-10 10:12:39 -0800

    Add session_actions, sessionReducer, usersReducer, and entitiesReducer

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -1,18 +1,42 @@
+import * AS SessionApiUtil from '../util/session_api_util';
+
 export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
 export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
 export const RECEIVE_ERRORS = 'RECEIVE_ERRORS';
 
-export const receiveCurrentUser = (currentUser) => ({
+export const receiveCurrentUser = currentUser => ({
   type: RECEIVE_CURRENT_USER,
-  currentUser
-})
+  currentUser,
+});
 
 export const logoutCurrentUser = () => ({
-  type: LOGOUT_CURRENT_USER
-})
+  type: LOGOUT_CURRENT_USER,
+});
 
-export const receiveErrors = (errors) => ({
-  type: LOGOUT_CURRENT_USER
-})
+export const receiveErrors = errors => ({
+  type: RECEIVE_ERRORS,
+  errors,
+});
+
+export const login = user => {
+  return (
+    SessionApiUtil.login(user)
+      .then(user => dispatch(receiveCurrentUser(user)))
+  )
+}
+
+export const logout = () => {
+  return (
+    SessionApiUtil.logout()
+      .then(() => dispatch(logoutCurrentUser()))
+  )
+}
+
+export const signup = user => {
+  return (
+    SessionApiUtil.signup(user)
+      .then(user => dispatch(receiveCurrentUser(user)))
+  )
+}
 
 
